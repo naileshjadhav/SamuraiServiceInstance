f88f27d15f7176275542362f5de06bea377498bd
nailesh jadhav <nailesh.jadhav@gmail.com>
2020-06-12 13:43:19 +0530

    Service instance project

---
+++ src/main/java/com/zensar/service/instance/SamuraiServiceInstanceApplication.java
@@ -0,0 +1,13 @@
+package com.zensar.service.instance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuraiServiceInstanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuraiServiceInstanceApplication.class, args);
+	}
+
+}
